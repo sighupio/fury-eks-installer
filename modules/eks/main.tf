d5ddd97c605436d6a4f4f894bc188eddde046540
Ángel Barrera Sánchez <angel@sighup.io>
2020-10-22 13:16:34 +0200

    Update provider dependencies versions

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.12.0"
   required_providers {
-    aws        = ">= 2.52.0"
-    kubernetes = ">= 1.11.1"
+    aws        = ">= 2.70.0"
+    kubernetes = ">= 1.13.2"
   }
 }

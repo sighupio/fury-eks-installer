0b581a57ee6d52860b6f61ba306b3ee384d8efcf
Ángel Barrera Sánchez <angel@sighup.io>
2020-12-10 09:42:41 +0100

    Add three specific variables to the eks module to manage the auth configmap

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -56,3 +56,52 @@ variable "resource_group_name" {
   description = "Resource group name where every resource will be placed. Required only in AKS installer (*)"
   default     = ""
 }
+
+variable "eks_map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap"
+  type        = list(string)
+
+  # example = [
+  #   "777777777777",
+  #   "888888888888",
+  # ]
+}
+
+variable "eks_map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap"
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  # example = [
+  #   {
+  #     rolearn  = "arn:aws:iam::66666666666:role/role1"
+  #     username = "role1"
+  #     groups   = ["system:masters"]
+  #   },
+  # ]
+}
+
+variable "eks_map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap"
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  # example = [
+  #   {
+  #     userarn  = "arn:aws:iam::66666666666:user/user1"
+  #     username = "user1"
+  #     groups   = ["system:masters"]
+  #   },
+  #   {
+  #     userarn  = "arn:aws:iam::66666666666:user/user2"
+  #     username = "user2"
+  #     groups   = ["system:masters"]
+  #   },
+  # ]
+}

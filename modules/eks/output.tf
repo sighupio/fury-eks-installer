9aaa33cf8fdc0c352ef7f4f2686568735e176343
Ángel Barrera Sánchez <angel@sighup.io>
2021-02-11 16:57:11 +0100

    Add a new output: cluster_oidc_issuer_url

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -12,3 +12,8 @@ output "operator_ssh_user" {
   description = "SSH user to access cluster nodes with ssh_public_key"
   value       = "ec2-user" # Default
 }
+
+output "eks_cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  value       = module.cluster.cluster_oidc_issuer_url
+}

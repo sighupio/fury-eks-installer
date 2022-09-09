729d4a409bb372b7e86e0db6f4b074d2a965f877
omissis <claudi0.beatric3@gmail.com>
2022-09-09 12:50:52 +0200

    chore: make examples running directly using terraform

--- example/eks/variables.tf
+++ example/eks/variables.tf
@@ -3,3 +3,8 @@ variable "cluster_name" {
   default     = "fury-example"
   description = "Unique cluster name. Used in multiple resources to identify your cluster resources"
 }
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key to be used to access the cluster nodes"
+}

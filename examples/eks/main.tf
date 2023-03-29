5b4431e793005259252cdc4b4f511a1e5bb21371
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-03-29 15:25:29 +0200

    docs: update docs and examples

--- examples/eks/main.tf
+++ examples/eks/main.tf
@@ -16,70 +16,34 @@ module "fury_example" {
   cluster_name    = var.cluster_name # make sure to use the same name you used in the VPC and VPN module
   cluster_version = "1.24"
 
-  vpc_id      = data.terraform_remote_state.vpc_and_vpn.outputs.vpc_id
-  subnetworks = data.terraform_remote_state.vpc_and_vpn.outputs.private_subnets
+  vpc_id  = data.terraform_remote_state.vpc_and_vpn.outputs.vpc_id
+  subnets = data.terraform_remote_state.vpc_and_vpn.outputs.private_subnets
 
   ssh_public_key                        = tls_private_key.ssh.private_key_pem
   cluster_endpoint_private_access_cidrs = data.terraform_remote_state.vpc_and_vpn.outputs.vpn_instances_private_ips_as_cidrs
 
-  node_pools = {
-    m5-node-pool = {
+  node_pools = [
+    {
+      name : "m5-node-pool"
       version : null # To use same value as cluster_version
       min_size : 1
       max_size : 2
       instance_type : "m5.large"
-      spot_instance : true
       volume_size : 100
-      subnetworks : null
-      eks_target_group_arns : null
+      subnets : null
+      target_group_arns : null
       additional_firewall_rules : {
         cidr_blocks = [
           {
             name : "Debug 1"
             type : "ingress"
-            cidr_block : "0.0.0.0/0"
+            cidr_blocks : ["0.0.0.0/0"]
             protocol : "TCP"
             from_port : 80
             to_port : 80
             tags : {
-              "hello" : "tag",
-              "cluster-tags" : "my-value-OVERRIDE-1"
-            }
-          }
-        ]
-      }
-      labels : {
-        "node.kubernetes.io/role" : "app"
-        "sighup.io/fury-release" : "v1.24.0"
-      }
-      taints : []
-      tags : {
-        "node-tags" : "exists"
-      }
-      # max_pods : null # To use default EKS setting set it to null or do not set it
-    }
-    m5-node-pool-spot : {
-      version : null # To use same value as cluster_version
-      min_size : 1
-      max_size : 2
-      instance_type : "m5.large"
-      spot_instance : true # optionally create spot instances
-      # os : "ami-0caf35bc73450c396" # optionally define a custom AMI
-      volume_size : 100
-      subnetworks : null
-      eks_target_group_arns : null
-      additional_firewall_rules : {
-        cidr_blocks = [
-          {
-            name : "Debug 2"
-            type : "ingress"
-            cidr_block : "0.0.0.0/0"
-            protocol : "TCP"
-            from_port : 80
-            to_port : 80
-            tags : {
-              "hello" : "tag",
-              "cluster-tags" : "my-value-OVERRIDE-2"
+              hello : "tag"
+              cluster-tags : "my-value-OVERRIDE-1"
             }
           }
         ]
@@ -94,7 +58,40 @@ module "fury_example" {
       }
       # max_pods : null # To use default EKS setting set it to null or do not set it
     },
-  }
+    {
+      name : "m5-node-pool-spot"
+      min_size : 1
+      max_size : 2
+      instance_type : "m5.large"
+      spot_instance : true # optionally create spot instances
+      # ami_id : "ami-01eb5348cab8e4902" # optionally define a custom AMI
+      volume_size : 100
+      additional_firewall_rules : {
+        cidr_blocks = [
+          {
+            name : "Debug 2"
+            type : "ingress"
+            cidr_blocks : ["0.0.0.0/0"]
+            protocol : "TCP"
+            from_port : 80
+            to_port : 80
+            tags : {
+              hello : "tag"
+              cluster-tags : "my-value-OVERRIDE-2"
+            }
+          }
+        ]
+      }
+      labels : {
+        "node.kubernetes.io/role" : "app"
+        "sighup.io/fury-release" : "v1.24.0"
+      }
+      tags : {
+        "node-tags" : "exists"
+      }
+      # max_pods : null # To use default EKS setting set it to null or do not set it
+    },
+  ]
 
   tags = {
     Environment : "kfd-development"

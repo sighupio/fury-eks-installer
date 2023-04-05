38a82967ece4727a626adc16582b4cd396221d89
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-04-05 17:15:34 +0200

    fix: tweak tf deps versions, add missing nls at eof

--- examples/eks-private/main.tf
+++ examples/eks-private/main.tf
@@ -5,12 +5,12 @@
  */
 
 terraform {
-  required_version = ">=0.15.4"
+  required_version = "~> 0.15.4"
   required_providers {
-    local    = "2.0.0"
-    null     = "3.0.0"
-    aws      = "3.56.0"
-    external = "2.0.0"
+    aws      = "~> 3.56.0"
+    external = "~> 2.0.0"
+    local    = "~> 2.0.0"
+    null     = "~> 3.0.0"
   }
 }
 

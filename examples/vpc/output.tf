5c8fad52f283098a1eefa2e8009072419aeb6530
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-04-05 12:52:53 +0200

    docs(vpc,example): add missing output to example

--- examples/vpc/output.tf
+++ examples/vpc/output.tf
@@ -12,7 +12,11 @@ output "private_subnets" {
   value       = module.vpc.private_subnets
 }
 
-
 output "nat_public_ips" {
   value = module.vpc.nat_public_ips
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
 }

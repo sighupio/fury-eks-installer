e9e25af7213f605d7af75710952c572b00a1496b
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-04-05 11:15:14 +0200

    fix(example,eks): update eks example

--- examples/eks/main.tf
+++ examples/eks/main.tf
@@ -1,7 +1,14 @@
-data "terraform_remote_state" "vpc_and_vpn" {
+data "terraform_remote_state" "vpc" {
   backend = "local"
   config = {
-    path = "${path.module}/../vpc-and-vpn/terraform.tfstate"
+    path = "${path.module}/../vpc/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "vpn" {
+  backend = "local"
+  config = {
+    path = "${path.module}/../vpn/terraform.tfstate"
   }
 }
 
@@ -13,14 +20,17 @@ resource "tls_private_key" "ssh" {
 module "fury_example" {
   source = "../../modules/eks"
 
-  cluster_name    = "fury-example"  # make sure to use the same name you used in the VPC and VPN module
-  cluster_version = "1.25"
+  cluster_name               = "fury-example"  # make sure to use the same name you used in the VPC and VPN module
+  cluster_version            = "1.25"
+  cluster_log_retention_days = 1
 
-  vpc_id  = data.terraform_remote_state.vpc_and_vpn.outputs.vpc_id
-  subnets = data.terraform_remote_state.vpc_and_vpn.outputs.private_subnets
+  availability_zone_names = ["eu-west-1a", "eu-west-1b"]
+  subnets                 = data.terraform_remote_state.vpc.outputs.private_subnets
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  cluster_endpoint_private_access_cidrs = data.terraform_remote_state.vpn.outputs.vpn_instances_private_ips_as_cidrs
 
   ssh_public_key                        = tls_private_key.ssh.private_key_pem
-  cluster_endpoint_private_access_cidrs = data.terraform_remote_state.vpc_and_vpn.outputs.vpn_instances_private_ips_as_cidrs
 
   node_pools = [
     {
@@ -32,6 +42,7 @@ module "fury_example" {
       volume_size : 100
       subnets : null
       target_group_arns : null
+      container_runtime = "containerd"
       additional_firewall_rules : {
         cidr_blocks = [
           {
@@ -66,6 +77,7 @@ module "fury_example" {
       spot_instance : true # optionally create spot instances
       # ami_id : "ami-01eb5348cab8e4902" # optionally define a custom AMI
       volume_size : 100
+      container_runtime = "docker"
       additional_firewall_rules : {
         cidr_blocks = [
           {
@@ -89,7 +101,14 @@ module "fury_example" {
       tags : {
         "node-tags" : "exists"
       }
-      # max_pods : null # To use default EKS setting set it to null or do not set it
+      max_pods : 35
+    },
+    {
+      name : "m5-node-pool-min-config"
+      min_size : 1
+      max_size : 2
+      instance_type : "m5.large"
+      volume_size : 10
     },
   ]
 
